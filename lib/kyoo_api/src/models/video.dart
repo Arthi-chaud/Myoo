f634efb74c27f0119ab1d6f5acdb0156eb3903f3
Arthi-chaud <arthur.jamet@gmail.com>
2022-03-12 12:13:19 +0100

    Kyoo API: Wrapper prototype for a vide ressource

---
+++ lib/kyoo_api/src/models/video.dart
@@ -0,0 +1,8 @@
+import 'package:myoo/kyoo_api/src/models/illustrated_ressource.dart';
+import 'package:myoo/kyoo_api/src/models/ressource.dart';
+
+/// A [ressource] that has duration
+class Video extends IllustratedRessource {
+  /// The length of the ressource
+  Duration duration;
+}
