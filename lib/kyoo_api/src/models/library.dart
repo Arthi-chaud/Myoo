f09415969ba419a88a76743401fe366a6a412aa0
Arthi-chaud <arthur.jamet@gmail.com>
2022-03-13 15:44:32 +0100

    Kyoo API: Library data model

---
+++ lib/kyoo_api/src/models/library.dart
@@ -0,0 +1,7 @@
+import 'package:myoo/kyoo_api/kyoo_api.dart';
+
+/// A Librayr holds [Movie]s, [Collection]s, and/or [TVSeries]
+class Library extends Ressource {
+  /// The content of the library
+  List<IllustratedRessource> content;
+}
