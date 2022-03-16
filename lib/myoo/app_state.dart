45d7fa8c23e3ab23c4494a87316295ea33963769
Arthi-chaud <arthur.jamet@gmail.com>
2022-03-16 13:50:46 +0100

    App State: add field for current client

--- lib/myoo/app_state.dart
+++ lib/myoo/app_state.dart
@@ -1,10 +1,12 @@
 import 'package:myoo/kyoo_api/kyoo_api.dart';
 import 'package:myoo/kyoo_api/src/kyoo_client.dart';
 import 'package:myoo/kyoo_api/src/models/ressource_preview.dart';
-
+/// State of the Myoo App
 class AppState {
   /// List of clients in the current state
   final List<KyooClient> clients;
+  /// The current [KyooClient] from which the requests must be made with
+  final KyooClient currentClient;
   /// Array of [ressourcePreview], for example in list view
   final List<RessourcePreview>? previews;
   /// Current [TVSeries]
@@ -20,21 +22,26 @@ class AppState {
   AppState({
     required this.isLoading,
     required this.clients,
+    required this.currentClient,
     required this.previews,
     required this.currentMovie,
     required this.currentSeries,
     required this.currentSeason
-  });
+  }) {
+    assert(clients.contains(currentClient), 'The current client is not known');
+  }
 
   /// Copy constructor for easier copies
   AppState copyWith({
     List<KyooClient>? clients,
+    KyooClient? currentClient,
     List<RessourcePreview>? previews,
     TVSeries? currentSeries,
     Season? currentSeason,
     Movie? currentMovie,
     bool? isLoading,
   }) => AppState(
+    currentClient: currentClient ?? this.currentClient,
     isLoading: isLoading ?? this.isLoading,
     clients: clients ?? this.clients,
     previews: previews ?? this.previews,
