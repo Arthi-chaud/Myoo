4ce4dff0550801ac120229066cc99d8441f969d3
Arthi-chaud <arthur.jamet@gmail.com>
2022-03-16 11:02:43 +0100

    ThemeData: Create custom color scheme$

---
+++ lib/myoo/theme_data.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0x000d1323);
+
+const ColorScheme colorScheme = ColorScheme(
+  primary: primaryColor,
+  secondary: Color(0x00E23C00),
+  background: primaryColor,
+  brightness: Brightness.dark,
+  onError: Colors.red,
+  error: Colors.red,
+  onPrimary: Colors.white,
+  onSecondary: Colors.white,
+  onBackground: Colors.white,
+  surface: Color(0x00C4C4C4),
+  onSurface: Color(0x00717171),
+);
