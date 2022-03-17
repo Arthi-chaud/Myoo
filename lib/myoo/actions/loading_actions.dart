7226f950bd21c34f7581bd788e821d0e60b9f406
Arthi-chaud <arthur.jamet@gmail.com>
2022-03-17 13:36:14 +0100

    State: loading state: create related actions

---
+++ lib/myoo/actions/loading_actions.dart
@@ -0,0 +1,7 @@
+import 'package:myoo/myoo/actions/action_model.dart';
+
+/// Action to set Loading state at true
+class LoadAction extends Action {}
+
+/// Action to set Loading state at false
+class LoadedAction extends Action {}
