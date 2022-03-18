5ae0589d05acf8b4ff5c502cde76b9892d8c1cbd
Arthi-chaud <arthur.jamet@gmail.com>
2022-03-18 12:07:59 +0100

    Init Page: Using store to load local clients and redirect to page

--- lib/myoo/src/views/init_page.dart
+++ lib/myoo/src/views/init_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:myoo/myoo/src/actions/client_actions.dart';
+import 'package:myoo/myoo/src/app_state.dart';
 import 'package:myoo/myoo/src/widgets/loading_widget.dart';
 
 /// 'Lobby' Page while initiating [AppState]
@@ -7,6 +10,17 @@ class InitializationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: LoadingWidget());
+    return Scaffold(
+      body: StoreBuilder<AppState>(
+        onInitialBuild: (store) => store.dispatch(LoadStoredClientsAction()),
+        builder: (context, store) {
+          if (store.state.isLoading == false) {
+           Future.delayed(const Duration(seconds: 2))
+            .then((value) => Navigator.of(context).pushNamed('/login'));
+          }
+          return const LoadingWidget();
+        }
+      )
+    );
   }
 }
