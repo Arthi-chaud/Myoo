6912e610529b48553d541bfe9209696e8f240696
Arthi-chaud <arthur.jamet@gmail.com>
2022-03-18 13:39:51 +0100

    Init Page: Use 'curretnClient' from State to get redirct page

--- lib/myoo/src/views/init_page.dart
+++ lib/myoo/src/views/init_page.dart
@@ -3,10 +3,17 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:myoo/myoo/src/actions/client_actions.dart';
 import 'package:myoo/myoo/src/app_state.dart';
 import 'package:myoo/myoo/src/widgets/loading_widget.dart';
+import 'package:redux/redux.dart';
 
 /// 'Lobby' Page while initiating [AppState]
 class InitializationPage extends StatelessWidget {
   const InitializationPage({Key? key}) : super(key: key);
+
+  void onLoaded(Store<AppState> store, BuildContext context) {
+    final route = store.state.currentClient == null ? '/login' : '/home';
+    Future.delayed(const Duration(seconds: 2))
+      .then((value) => Navigator.of(context).pushNamed(route));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,8 +22,7 @@ class InitializationPage extends StatelessWidget {
         onInitialBuild: (store) => store.dispatch(LoadStoredClientsAction()),
         builder: (context, store) {
           if (store.state.isLoading == false) {
-           Future.delayed(const Duration(seconds: 2))
-            .then((value) => Navigator.of(context).pushNamed('/login'));
+            onLoaded(store, context);
           }
           return const LoadingWidget();
         }
