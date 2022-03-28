8089d8687bb6edbdc630b887a3237ff2766af63d
Arthi-chaud <arthur.jamet@gmail.com>
2022-03-28 09:46:59 +0200

    TVSeries Detail Page: rearrange season's name for better padding

--- lib/myoo/src/views/detail_views/tvseries_page.dart
+++ lib/myoo/src/views/detail_views/tvseries_page.dart
@@ -1,4 +1,5 @@
 import 'package:expandable_text/expandable_text.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:myoo/kyoo_api/kyoo_api.dart';
 import 'package:myoo/myoo/src/app_state.dart';
@@ -17,7 +18,13 @@ class TVSeriesPage extends StatelessWidget {
       isLoading: (store) => store.state.currentTVSeries == null,
       builder: (context, store) {
         TVSeries tvSeries = store.state.currentTVSeries!;
+        int initialSeasonIndex = 0;
+        if (tvSeries.seasons.first.name.toLowerCase().contains("specials") || // If specials
+          tvSeries.seasons.first.name.toLowerCase().endsWith(" 0")) { // if season 0
+          initialSeasonIndex = 1;
+        }
         return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             DetailPageHeader(
               thumbnailURL: tvSeries.thumbnail,
@@ -32,17 +39,36 @@ class TVSeriesPage extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.only(left: 25, right: 25, bottom: 10),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 30),
-                child: ExpandableText(
-                  tvSeries.overview,
-                  expandText: 'Show more',
-                  maxLines: 5,
-                  linkColor: getColorScheme(context).onBackground,
-                  linkStyle: const TextStyle(fontWeight: FontWeight.bold),
-                ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 30),
+                    child: ExpandableText(
+                      tvSeries.overview,
+                      expandText: 'Show more',
+                      maxLines: 5,
+                      linkColor: getColorScheme(context).onBackground,
+                      linkStyle: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  if (tvSeries.seasons.length == 1)
+                  Text(tvSeries.seasons.first.name),
+                ],
               ),
             ),
+            if (tvSeries.seasons.length > 1)
+            DefaultTabController(
+              length: tvSeries.seasons.length,
+              initialIndex: initialSeasonIndex,
+              child: TabBar(
+                isScrollable: true,
+                indicatorColor: getColorScheme(context).secondary,
+                tabs: tvSeries.seasons.map(
+                  (season) => Tab(text: season.name)
+                ).toList()
+              ),
+            )
           ],
         );
       }
