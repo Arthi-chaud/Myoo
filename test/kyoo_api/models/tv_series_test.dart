019d64f5ecf80dd547a7d32e08b00175b6dd7c4a
Arthi-chaud <arthur.jamet@gmail.com>
2022-04-11 14:44:50 +0200

    Kyoo API: TV Series: Rewrite unserialization test

--- test/kyoo_api/models/tv_series_test.dart
+++ test/kyoo_api/models/tv_series_test.dart
@@ -1,99 +1,34 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:myoo/kyoo_api/kyoo_api.dart';
 import 'package:test/test.dart';
 
 void main() {
-  var now = DateTime.now();
-  var genres = ['Action', 'Adventure', "Express"];
   group('TVSeries', () {
-    test('Default constructor', () {
-      TVSeries resource = TVSeries(
-          id: 1,
-          slug: 'slug',
-          name: 'name',
-          overview: 'overview',
-          poster: 'poster',
-          thumbnail: 'thumbnail',
-          genres: genres,
-          trailer: "trailer",
-          endDate: null,
-          studio: null,
-          releaseDate: null,
-          seasons: []);
-      expect(resource.id, 1);
-      expect(resource.slug, 'slug');
-      expect(resource.name, 'name');
-      expect(resource.overview, 'overview');
-      expect(resource.poster, 'poster');
-      expect(resource.thumbnail, 'thumbnail');
-      expect(resource.seasons, []);
-    });
+    test('From JSON', () async {
+      JSONData jsonSeries = jsonDecode(await File('test/kyoo_api/assets/tv_series.json').readAsString());
+      TVSeries series = TVSeries.fromJson(jsonSeries);
 
-    test('Unserialize: All fields set', () {
-      JSONData input = {
-        'id': 12345,
-        'slug': 'mySlug2',
-        'name': 'myResource2',
-        'poster': 'poster',
-        'thumbnail': 'thumbnail',
-        'overview': 'overview',
-        'trailer': 'trailer',
-        'genres': [
-          {'id': 1, 'name': 'Action'},
-          {'id': 2, 'name': 'Adventure'},
-          {'id': 3, 'name': 'Express'}
-        ],
-        'seasons': [
-          {
-            'id': 12345,
-            'slug': 'mySlug2',
-            'name': 'myResource2',
-            'poster': 'poster',
-            'thumbnail': 'thumbnail',
-            'overview': 'overview',
-            'seasonNumber': 1,
-            'episodes': [
-              {
-                'id': 12345,
-                'slug': 'mySlug2',
-                'name': 'myResource2',
-                'thumbnail': 'thumbnail',
-                'overview': 'overview',
-                'releaseDate': now.toIso8601String(),
-                'absoluteNumber': 1,
-                'episodeNumber': 2
-              }
-            ]
-          }
-        ]
-      };
-      TVSeries resource = TVSeries.fromJson(input);
-      expect(resource.id, 12345);
-      expect(resource.slug, 'mySlug2');
-      expect(resource.name, 'myResource2');
-      expect(resource.overview, 'overview');
-      expect(resource.poster, 'poster');
-      expect(resource.thumbnail, 'thumbnail');
-      expect(resource.genres, genres);
-      expect(resource.trailer, 'trailer');
-      expect(resource.seasons.length, 1);
-      Season season = resource.seasons[0];
-      expect(season.id, 12345);
-      expect(season.index, 1);
-      expect(season.slug, 'mySlug2');
-      expect(season.name, 'myResource2');
-      expect(season.overview, 'overview');
-      expect(season.poster, 'poster');
-      expect(season.thumbnail, 'thumbnail');
-      expect(season.episodes.length, 1);
-      Episode episode = season.episodes[0];
-      expect(episode.id, 12345);
-      expect(episode.slug, 'mySlug2');
-      expect(episode.name, 'myResource2');
-      expect(episode.overview, 'overview');
-      expect(episode.releaseDate, now);
-      expect(episode.thumbnail, 'thumbnail');
-      expect(episode.absoluteIndex, 1);
-      expect(episode.index, 2);
+      expect(series.id, 3);
+      expect(series.slug, 'pioneer-one');
+      expect(series.name, "Pioneer One");
+      expect(series.overview,  "Pioneer One is a 2010 American web series produced by Josh Bernhard and Bracey Smith. It has been funded purely through donations, and is the first series created for and released on BitTorrent networks.");
+      expect(series.releaseDate, DateTime(2010, 6, 16));
+      expect(series.endDate, DateTime(2011, 12, 13));
+      expect(series.poster, '/api/show/pioneer-one/poster');
+      expect(series.thumbnail, "/api/show/pioneer-one/thumbnail");
+      expect(series.trailer, "https://www.youtube.com/watch?v=icLQqVDk86Y");
+      expect(series.studio, 'lastsat productions');
+      expect(series.staff, []);
+      expect(series.genres, ['Drama', 'Science Fiction']);
+      expect(series.seasons, []);
+
+      expect(series.externalIDs.length, 1);
+      expect(series.externalIDs.first.externalURL, 'https://www.thetvdb.com/series/pioneer-one');
+      expect(series.externalIDs.first.provider.id, 1);
+      expect(series.externalIDs.first.provider.slug, 'the-tvdb');
+      expect(series.externalIDs.first.provider.name, 'TheTVDB');
+      expect(series.externalIDs.first.provider.logo, "/api/provider/the-tvdb/logo");
     });
   });
 }
