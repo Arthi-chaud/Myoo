387c730139f00aa09393ebdcd706e24190c866f9
Arthi-chaud <arthur.jamet@gmail.com>
2022-04-11 15:40:08 +0200

    Kyoo PAI: Test: rewrite Season Unserialization

---
+++ test/kyoo_api/models/season_test.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:myoo/kyoo_api/src/models/season.dart';
+import 'package:myoo/kyoo_api/src/models/json.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Season', () async {
+    JSONData jsonSeason = jsonDecode(await File('test/kyoo_api/assets/season.json').readAsString());
+    Season season = Season.fromJson(jsonSeason);
+    test('From JSON', () {
+      expect(season.id, 285);
+      expect(season.slug, 'aggretsuko-s1');
+      expect(season.name, "Season 1");
+      expect(season.overview,  "Frustrated with her thankless office job, Retsuko the Red Panda copes with her daily struggles by belting out death metal karaoke after work.");
+      expect(season.poster, "/api/season/aggretsuko-s1/poster");
+      expect(season.thumbnail, null);
+      expect(season.index, 1);
+      expect(season.episodes, []);
+    });
+  });
+}
